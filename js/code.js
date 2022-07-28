b1fc15ed3a81e1b5df568aa9b0640e568aa782bd
Bablu111 <103333242+Bablu111@users.noreply.github.com>
2022-07-29 01:46:03 +0600

    problem

---
+++ js/code.js
@@ -0,0 +1,24 @@
+$(function () {
+
+    //*active logo here*/
+
+    $(".menu_icon").click(function () {
+
+        let logo = $("nav .logo");
+        let navMenu = $("#navbarSupportedContent ul");
+        logo.toggleClass("activeLogo");
+        navMenu.fadeToggle();
+    });
+
+    //* banner slide */
+
+    $('#banner').slick({
+        
+
+        
+      });
+
+
+
+
+});
